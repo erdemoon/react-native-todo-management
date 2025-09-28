21c58f4131bb0564c3cc018de8a199d671f92695
erdemoon <erdemali.ay@gmail.com>
2025-09-28 19:01:03 +0300

    layout changes

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,12 +2,39 @@ import '../global.css';
 
 import { Stack } from 'expo-router';
 
+import { Loading } from '@/components/Loading';
 import DatabaseProvider from '@/providers/database-provider';
 
 export default function Layout() {
   return (
     <DatabaseProvider>
-      <Stack />
+      <Stack
+        screenOptions={{
+          headerTransparent: true,
+          headerBackButtonDisplayMode: 'minimal',
+          headerLargeTitle: true,
+          headerTitleStyle: {
+            color: 'white',
+          },
+        }}>
+        <Stack.Screen
+          name="listModal"
+          options={{
+            title: 'New List',
+            headerLargeTitle: false,
+            presentation: 'modal',
+          }}
+        />
+        <Stack.Screen
+          name="taskModal"
+          options={{
+            title: 'New Task',
+            headerLargeTitle: false,
+            presentation: 'modal',
+          }}
+        />
+      </Stack>
+      <Loading />
     </DatabaseProvider>
   );
 }
