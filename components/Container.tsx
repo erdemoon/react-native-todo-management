678104cdf14c1a8a1d9a0cf19453d6af06728583
erdemoon <erdemali.ay@gmail.com>
2025-09-28 18:58:04 +0300

    container component changes

--- components/Container.tsx
+++ components/Container.tsx
@@ -1,9 +1,25 @@
-import { SafeAreaView } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { ScrollView, View } from 'react-native';
 
-export const Container = ({ children }: { children: React.ReactNode }) => {
-  return <SafeAreaView className={styles.container}>{children}</SafeAreaView>;
+export const Container = ({
+  children,
+  className = 'bg-black',
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) => {
+  return (
+    <View className="flex-1 bg-transparent">
+      <StatusBar style="light" />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        className={styles.container + ` ${className}`}>
+        {children}
+      </ScrollView>
+    </View>
+  );
 };
 
 const styles = {
-  container: 'flex flex-1 m-6',
+  container: 'flex flex-1 pb-6 pr-2 pl-2',
 };
